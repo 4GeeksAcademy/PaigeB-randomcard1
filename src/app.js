6b21c0fe2ba4c677cdc94d2f913fa6b07ef5df57
Techprenuer1 <125614142+Techprenuer1@users.noreply.github.com>
2023-04-27 02:38:35 +0000

    finished Random Card Project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  button();
+  cardGenerator();
+};
+let button = () => {
+  let getNewCard = document.querySelector(".btn");
+  getNewCard.addEventListener("click", function() {
+    cardGenerator();
+  });
+};
+let cardGenerator = () => {
+  let randomSuit = ["♦️", "♣️", "&#10084;", "♠️"];
+  let randomNumber = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+  let suit = randomSuit[Math.floor(Math.random() * randomSuit.length)];
+  let number = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+
+  if (suit == ":hearts:" || suit == ":diamonds:") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  } else {
+    document.querySelector(".top").style.color = "black";
+    document.querySelector(".bottom").style.color = "black";
+  }
+  document.querySelector(".top").innerHTML = suit;
+  document.querySelector(".number").innerHTML = number;
+  document.querySelector(".bottom").innerHTML = suit;
 };
